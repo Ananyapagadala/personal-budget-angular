3fc24250c1fd10996c82206f62d3362bbf041b29
Ananyapagadala <ananyapagadala27@gmail.com>
2025-02-09 01:08:04 -0500

    Added server, Axios, and ChartJS

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
-// Budget API
-
 const express = require('express');
-const cors = require('cors');
 const app = express();
 const port = 3000;
 
-app.use(cors());
+app.use('/',express.static('public'));
 
 const budget = {
     myBudget: [
@@ -25,10 +22,14 @@ const budget = {
 };
 
 
-app.get('/budget', (req, res) => {
+app.get('/hello', (req, res) =>{
+    res.send('Hello World!');
+});
+
+app.get('/budget', (req, res) =>{
     res.json(budget);
 });
 
-app.listen(port, () => {
-    console.log(`API served at http://localhost:${port}`);
+app.listen(port, () =>{
+    console.log(`Example applistening at http://localhost:${port}`)
 });
