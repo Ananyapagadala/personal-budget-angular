e0d2e3544a50ef2eff31acb57d6390b21a979e5e
Ananyapagadala <ananyapagadala27@gmail.com>
2025-02-16 14:38:11 -0500

    week 5 before canvas changes

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = 3000;
 
 app.use('/',express.static('public'));
+app.use(cors());
 const data = require('./data.json');
 
 const budget = {
