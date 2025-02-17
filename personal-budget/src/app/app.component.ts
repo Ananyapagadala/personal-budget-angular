6db0da455541514f14321fb0f16be3b17756b0f4
Ananyapagadala <ananyapagadala27@gmail.com>
2025-02-16 22:49:15 -0500

    Coding Exercise 2 (week 5)

--- personal-budget/src/app/app.component.ts
+++ personal-budget/src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { MenuComponent } from './menu/menu.component';
 import { HeroComponent } from './hero/hero.component';
 import { FooterComponent } from './footer/footer.component';
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'pb-root',
-  imports: [RouterOutlet, MenuComponent, HeroComponent,FooterComponent],
+  imports: [RouterOutlet, MenuComponent, HeroComponent,FooterComponent,ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
